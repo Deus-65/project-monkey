b3e606bb30b280ec280d59ca63638025bc77af0d
Deus-65 <demirman52@hotmail.com>
2026-04-05 23:12:04 +0300

    Camera has been added

---
+++ project-monkey/Assets/Scripts/PlayerCam.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerCam : MonoBehaviour
+{
+    public float sensX;
+    public float sensY;
+
+    public Transform orientation;
+
+    float xRotation;
+    float yRotation;
+
+    private void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+    private void Update()
+    {
+        /* get mouse input old way that doesn't work
+        float mouseX = Input.GetAxisRaw("mouse X") * Time.deltaTime * sensX;
+        float mouseY = Input.GetAxisRaw("mouse Y") * Time.deltaTime * sensY;
+        */
+        
+        // Get mouse delta using the New Input System
+        Vector2 mouseDelta = Mouse.current.delta.ReadValue();
+
+        float mouseX = mouseDelta.x * Time.deltaTime * sensX;
+        float mouseY = mouseDelta.y * Time.deltaTime * sensY;
+
+
+        yRotation += mouseX;
+
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+        // Rotate cam and orientation
+        transform.rotation = Quaternion.Euler(xRotation, yRotation, 0);
+        orientation.rotation = Quaternion.Euler(0, yRotation, 0);
+
+    }
+}
